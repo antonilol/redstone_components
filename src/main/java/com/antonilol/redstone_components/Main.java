936ba0115ba207ea83230395e569b9a6a9a713c5
Antoni Spaanderman <56turtle56@gmail.com>
2022-01-03 16:29:52 +0100

    v1.2.0

--- src/main/java/com/antonilol/redstone_components/Main.java
+++ src/main/java/com/antonilol/redstone_components/Main.java
@@ -90,7 +90,7 @@ public class Main implements ModInitializer {
 
 	public static final String MOD_ID = "redstone_components";
 
-	public static final String VERSION = "1.1.1"; // updated by updateVersion script with sed :)
+	public static final String VERSION = "1.2.0"; // updated by updateVersion script with sed :)
 
 	@Override
 	public void onInitialize() {
